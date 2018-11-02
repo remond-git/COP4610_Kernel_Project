24b2c5d1931389974ff2ae7cc5aadc80f874c695
Bloopyloop <32317506+Bloopyloop@users.noreply.github.com>
2018-11-02 10:07:53 -0400

    Added couple more declars for nessesary funcs

--- part3/include/syscalls.h
+++ part3/include/syscalls.h
@@ -6,7 +6,13 @@ void elevator_syscalls_remove(void);
 void initQueue(void);
 void queuePassenger(int type, int start, int end);
 void PrintQueue(void);
-int passengWeights(int type);
+int passengWeights(int type); //returns weight for type
+int passengQueueSize(int floor); //counts entries in queue
+int passengQueueWeights(int floor); //counts total weight in queue
 int elevatorMove(int floor);
+int elevatorWeight(void); //weight of elevator
+int elevatorRun(void* data); //Actually runs the elevator
+int ifLoad(void); //If load should occur, returns 1 if true, returns 0 otherwise
+int ifUnload(void); //If unload should occur, returns 1 true, returns 0 otherwise
 
 #endif
