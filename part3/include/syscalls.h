c461328f7f9ec4f4efa0dc9e7d5e02b8cb2fdb10
Bloopyloop <32317506+Bloopyloop@users.noreply.github.com>
2018-11-02 17:53:35 -0400

    Update syscalls.h

--- part3/include/syscalls.h
+++ part3/include/syscalls.h
@@ -10,9 +10,10 @@ int passengWeights(int type); //returns weight for type
 int passengQueueSize(int floor); //counts entries in queue
 int passengQueueWeights(int floor); //counts total weight in queue
 int elevatorMove(int floor);
-int elevatorWeight(void); //weight of elevator
-int elevatorRun(void* data); //Actually runs the elevator
+int elevWeight(void); //weight of elevator
+int elevatorRun(void); //Actually runs the elevator
 int ifLoad(void); //If load should occur, returns 1 if true, returns 0 otherwise
 int ifUnload(void); //If unload should occur, returns 1 true, returns 0 otherwise
+void unloadPassengers(void); //Unloads each passenger (frees them)
 
 #endif
