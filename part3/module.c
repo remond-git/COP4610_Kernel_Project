901d927b1fe6a40c3206badbc153312410418c8e
Andrew Stringfellow <AndStringfellow@gmail.com>
2018-11-03 18:20:31 -0400

    Add part3/module.c

---
+++ part3/module.c
@@ -0,0 +1,33 @@
+#include <linux/module.h> // Dynamic loading of modules into the kernel.
+#include <linux/init.h> // Macros for module initialization.
+#include <linux/prof_fs.h> // File system structure and calls.
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("An elevator simulator utilizing first-in, first-out scheduling.");
+
+#define MODULE_NAME "Elevator"
+#define MODULE_PERMISSIONS 0644
+#define MODULE_PARENT NULL
+
+static struct file_operations fileOperations; // Points to proc file definitions.
+
+static in InitializeModule(void) {
+    printk(KERN_NOTICE "Creating /proc/%s.\n", MODULE_NAME);
+
+    if (!proc_create(MODULE_NAME, MODULE_PERMISSIONS, MODULE_PARENT, &fileOperations)) {
+        printk(KERN_WARNING "Error: could not create proc entry.\n");
+        remove_proc_entry(MODULE_NAME, MODULE_PARENT);
+
+        return -ENOMEM;
+    }
+    
+    return 0;
+}
+
+static void ExitModule(void) {
+    printk(KERN_NOTICE "Removing /proc/%s.\n", MODULE_NAME);
+    remove_proc_entry(MODULE_NAME, MODULE_PARENT);
+}
+
+module_init(InitializeModule);
+module_exit(ExitModule);
