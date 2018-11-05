b5869ca6b11e1d8cc9b5b2a079dc3eb6b53adc77
Bloopyloop <32317506+Bloopyloop@users.noreply.github.com>
2018-11-05 14:35:20 -0500

    Add files via upload

--- part3/include/syscalls.h
+++ part3/include/syscalls.h
@@ -6,14 +6,17 @@ void elevator_syscalls_remove(void);
 void initQueue(void);
 void queuePassenger(int type, int start, int end);
 void PrintQueue(void);
+char* queueToString(void);
 int passengWeights(int type); //returns weight for type
-int passengQueueSize(int floor); //counts entries in queue
-int passengQueueWeights(int floor); //counts total weight in queue
+int passengerQueueSize(int floor); //counts size in queue of floor
+int passengerQueueWeight(int floor); //counts total weight in queue of floor
 int elevatorMove(int floor);
 int elevWeight(void); //weight of elevator
+int elevSize(void);
 int elevatorRun(void); //Actually runs the elevator
 int ifLoad(void); //If load should occur, returns 1 if true, returns 0 otherwise
 int ifUnload(void); //If unload should occur, returns 1 true, returns 0 otherwise
+void loadPassenger(int floor);
 void unloadPassengers(void); //Unloads each passenger (frees them)
 
 #endif
